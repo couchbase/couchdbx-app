4e5048c39e9706b99809329ae62f4dc0464686dc
Dustin Sallings <dustin@spy.net>
2011-03-10 20:14:04 -0800

    Script to fix lib and bin paths after copy.
    Change-Id: I8f18baff40fe560ca0209205a9d8131485d0551a
    Reviewed-on: http://review.membase.org/4865
    Tested-by: Dustin Sallings <dustin@spy.net>
    Reviewed-by: Dustin Sallings <dustin@spy.net>

---
+++ fix-paths.sh
@@ -0,0 +1,40 @@
+#!/bin/sh -e
+
+instdir=${SRCROOT%dependencies/couchdbx-app}
+builddir=${instdir}build/
+
+dest=$BUILT_PRODUCTS_DIR/$UNLOCALIZED_RESOURCES_FOLDER_PATH/couchdbx-core
+
+clean_lib() {
+    while read something
+    do
+        install_name_tool -change $instdir$something $something $1
+        install_name_tool -change $builddir$something $something $1
+    done
+}
+
+# Find and cleanup all libs.
+for l in $dest/lib/*.dylib $dest/lib/couchdb/erlang/lib/couch-*/priv/lib/couch_icu_driver.so
+do
+    otool -L $l | grep "$instdir" \
+        | sed -e 's/(\(.*\))//g' -e "s,${instdir}build/,," | clean_lib $l
+done
+
+absolutize() {
+        # change absolute paths to dynamic absolute paths
+        echo absolutifying $1
+        perl -pi -e "s@$builddir@\`pwd\`/@" $dest/$1
+}
+
+relativize() {
+        # change absolute paths to dynamic absolute paths
+        echo relativizing $1
+        perl -pi -e "s@$builddir@@" $dest/$1
+}
+
+for f in bin/erl bin/js-config bin/icu-config bin/couchdb bin/couchjs
+do
+    absolutize $f
+done
+
+relativize etc/couchdb/default.ini
