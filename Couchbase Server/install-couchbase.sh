33a3aebe7006ef0082ccf635c31b8d844f760628
Chris Hillery <ceej@couchbase.com>
2015-04-14 17:24:39 -0700

    MB-14469: Skip healthchecker (no longer included in product)
    Change-Id: Ic20aa9cc013f2def323d05d87e11d79f7ef8d617
    Reviewed-on: http://review.couchbase.org/49705
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

--- Couchbase Server/install-couchbase.sh
+++ Couchbase Server/install-couchbase.sh
@@ -50,7 +50,6 @@ _fix_python_path "bin/cbvbucketctl"             ".."
 _fix_python_path "bin/cbworkloadgen"            ".."
 _fix_python_path "bin/couchbase-cli"            ".."
 _fix_python_path "bin/cbrecovery"               ".."
-_fix_python_path "bin/cbhealthchecker"          ".."
 _fix_python_path "bin/cbdocloader"              ".."
 
 _fix_python_path "bin/tools/cbanalyze-core"     "../.."
